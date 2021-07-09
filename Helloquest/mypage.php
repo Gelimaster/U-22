5595b838c573d6db2f60cbcbe29547a1287daf65
Gelimaster <50572505+Gelimaster@users.noreply.github.com>
2021-07-09 17:49:12 +0900

    [patch0.1]start

---
+++ Helloquest/mypage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Web Quest</title>
+
+    <link rel="stylesheet" type="text/css" href="css/mypage.css">
+
+  </head>
+
+  <body>
+
+    <header>
+      <img src="images/mypage/logo.png" alt="ロゴ" class="logo">
+    </header>
+
+
+    <a href="profile.php" class="button">プロフィール</a>
+    <a href="list.php" class="button">アイテム</a>
+    <a href="shop.php" class="button">ショップ</a>
+    <a href="customize.php" class="button">カスタマイズ</a>
+    <a href="koukai.php" class="button">公開ページ一覧</a>
+
+    <a href="quest.php" class="quest">クエスト</a>
+
+  </body>
+
+</html>
---
+++ Helloquest/profile.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <title>Hello Web Quest</title>
+
+    <link rel="stylesheet" type="text/css" href="css/profile.css">
+
+  </head>
+
+  <body>
+
+      <header>
+          <img src="images/profile/logo.png" alt="ロゴ" class="logo">
+          <h1><font size="20">プロフィール</font></h1>
+      </header>
+
+
+
+    <form action="#" method="post">
+	    <p><font size="5">ユーザー名：</font>
+	      <input type="text" name="name" placeholder="ユーザー名"></p>
+      <p><font size="5">性別：</font>
+        <input type="radio" name="q1" value="男性"> 男
+        <input type="radio" name="q1" value="女性"> 女
+      </p>
+      <p><font size="5">コメント：</font>
+    	  <textarea name="comment" cols="40" rows="8" placeholder="コメント"></textarea >
+    	</p>
+    </form>
+
+
+    <a href="profile.php"><button class="button1">決定</button></a>
+    <a href="mypage.php"><button class="button2">戻る</button></a>
+
+  </body>
+
+</html>
