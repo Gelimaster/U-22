7fd20d6027b2dfbbc870784b680275a5fcc7d695
Gelimaster <50572505+Gelimaster@users.noreply.github.com>
2021-07-12 10:03:47 +0900

    [追加]仲さんの公開ページ追加

---
+++ Helloquest/koukai.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Web Quest</title>
+    <link rel="stylesheet" type="text/css" href="css/koukai.css">
+  </head>
+  <body>
+    <header>
+      <a href="mypage.php">
+        <img style="float: left;" src="images/koukai/back.png" alt="戻るボタン" class="back">
+      </a>
+      <p class="subtitle">公開ページ一覧</p>
+    </header>
+    <table>
+      <tr>
+        <th>ここにmypage</th>
+        <th>ここにmypage</th>
+      </tr>
+      <tr class="under">
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+
+
+      <tr>
+        <th>ここにmypage</th>
+        <th>ここにmypage</th>
+      </tr>
+      <tr class="under">
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+
+
+
+      <tr>
+        <th>ここにmypage</th>
+        <th>ここにmypage</th>
+      </tr>
+      <tr>
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+      
+      
+      
+      <tr>
+        <th>ここにmypage</th>
+        <th>ここにmypage</th>
+      </tr>
+      <trclass="under">
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+      
+      
+      
+      <tr>
+        <th>ここにmypage</th>
+        <th>ここにmypage</th>
+      </tr>
+      <tr>
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+    </table>
+    
+
+  </body>
+</html>
