7d8dd4955ad2993de526b1357994615f23592c69
kotaro4014 <kotaro.stitch.19984014@gmail.com>
2021-07-16 19:42:24 +0900

    アイテムとクエストのtutorialを作りました

---
+++ Tutorial/item/site/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Web Quest</title>
+
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+
+  </head>
+
+  <body>
+
+    <header>
+      <img src="images/logo.png" alt="ロゴ" class="logo">
+    </header>
+
+
+    <a href="profiel.html" class="button">プロフィール</a>
+    <a href="item.html" class="item">アイテム</a>
+    <a href="shop.html" class="button">ショップ</a>
+    <a href="customize.html" class="button">カスタマイズ</a>
+    <a href="koukai.html" class="button">公開ページ一覧</a>
+
+    <a href="quest.html" class="quest">クエスト</a>
+
+
+
+    <div class="div">
+
+    </div>
+
+
+  </body>
+
+</html>
