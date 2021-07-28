beeed4d2b681044770e71ba02fd6b88140651aa6
Gelimaster <50572505+Gelimaster@users.noreply.github.com>
2021-07-28 16:03:15 +0900

    [更新]koukaiページby仲さん

---
+++ Frontend/koukai/site/koukai.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Web Quest</title>
+    
+    <link rel="stylesheet" type="text/css" href="css/koukai.css">
+ 
+  </head>
+  <body>
+    <header>
+      <a href="mypage.php">
+        <img style="float: left;" src="images/back.png" alt="戻るボタン" class="back">
+      </a>
+      <p class="subtitle">公開ページ一覧</p>
+    </header>
+
+    <!-- <div class="display">
+      <iframe src="mypage.php"  style="vertical-align: bottom" scrolling="no" ></iframe>
+      <iframe src="mypage.php"  style="transform:scale(0.55);-o-transform:scale(0.55);-webkit-transform:scale(0.55);-moz-transform:scale(0.55);-ms-transform:scale(0.55);"></iframe>
+    </div> -->
+    <div class="display">
+      <div class="display-inside" >
+        <div class="display-content" >
+          <iframe src="../../mypage/site/index.php" frameborder="0" scrolling="no" style="transform:scale(0.55);-o-transform:scale(0.55);-webkit-transform:scale(0.55);-moz-transform:scale(0.55);-ms-transform:scale(0.55);transform-origin:0 0;-o-transform-origin:0 0;-webkit-transform-origin:0 0;-moz-transform-origin:0 0;-ms-transform-origin:0 0;"></iframe>
+        </div>
+      </div>
+      <p class="display-username">ああああああああああ</p>
+    </div>
+        
+    <div class="display">
+      <div class="display-inside" >
+        <div class="display-content" >
+          <iframe src="../../mypage/site/index.php" frameborder="0" scrolling="no" style="transform:scale(0.55);-o-transform:scale(0.55);-webkit-transform:scale(0.55);-moz-transform:scale(0.55);-ms-transform:scale(0.55);transform-origin:0 0;-o-transform-origin:0 0;-webkit-transform-origin:0 0;-moz-transform-origin:0 0;-ms-transform-origin:0 0;"></iframe>
+        </div>
+      </div>
+      <p class="display-username">ああああああああああ</p>
+    </div>
+
+    <div class="display">
+      <div class="display-inside" >
+        <div class="display-content" >
+          <iframe src="../../mypage/site/index.php" frameborder="0" scrolling="no" style="transform:scale(0.55);-o-transform:scale(0.55);-webkit-transform:scale(0.55);-moz-transform:scale(0.55);-ms-transform:scale(0.55);transform-origin:0 0;-o-transform-origin:0 0;-webkit-transform-origin:0 0;-moz-transform-origin:0 0;-ms-transform-origin:0 0;"></iframe>
+        </div>
+      </div>
+      <p class="display-username">ああああああああああ</p>
+    </div>
+    
+    <div class="display">
+      <div class="display-inside" >
+        <div class="display-content" >
+          <iframe src="../../mypage/site/index.php" frameborder="0" scrolling="no" style="transform:scale(0.55);-o-transform:scale(0.55);-webkit-transform:scale(0.55);-moz-transform:scale(0.55);-ms-transform:scale(0.55);transform-origin:0 0;-o-transform-origin:0 0;-webkit-transform-origin:0 0;-moz-transform-origin:0 0;-ms-transform-origin:0 0;"></iframe>
+        </div>
+      </div>
+      <p class="display-username">ああああああああああ</p>
+    </div>
+
+    <div class="display">
+      <div class="display-inside" >
+        <div class="display-content" >
+          <iframe src="../../mypage/site/index.php" frameborder="0" scrolling="no" style="transform:scale(0.55);-o-transform:scale(0.55);-webkit-transform:scale(0.55);-moz-transform:scale(0.55);-ms-transform:scale(0.55);transform-origin:0 0;-o-transform-origin:0 0;-webkit-transform-origin:0 0;-moz-transform-origin:0 0;-ms-transform-origin:0 0;"></iframe>
+        </div>
+      </div>
+      <p class="display-username">ああああああああああ</p>
+    </div>
+
+    <!-- <table>
+      <tr>
+        <th> </th>
+        <th>ここにmypage</th>
+      </tr>
+      <tr class="under">
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+
+
+      <tr>
+        <th>ここにmypage</th>
+        <th>ここにmypage</th>
+      </tr>
+      <tr class="under">
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+
+
+
+      <tr>
+        <th>ここにmypage</th>
+        <th>ここにmypage</th>
+      </tr>
+      <tr>
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+      
+      
+      
+      <tr>
+        <th>ここにmypage</th>
+        <th>ここにmypage</th>
+      </tr>
+      <trclass="under">
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+      
+      
+      
+      <tr>
+        <th>ここにmypage</th>
+        <th>ここにmypage</th>
+      </tr>
+      <tr>
+        <td>ここに名前</td>
+        <td>ここに名前</td>
+      </tr>
+    </table> -->
+    
+
+  </body>
+</html>
