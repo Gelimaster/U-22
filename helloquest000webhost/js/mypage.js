d6ad1b6f8fd3bb8d4488802bbf345750f435e26e
Gelimaster <50572505+Gelimaster@users.noreply.github.com>
2021-08-15 15:04:05 +0900

    [直し]shop/customを1-2クリア後で開放する処理

--- helloquest000webhost/js/mypage.js
+++ helloquest000webhost/js/mypage.js
@@ -1,5 +1,12 @@
 //最後に入ったページにセットする
 localStorage.setItem("path_url","/mypage.php")
+    var shop = document.getElementById("link3000")
+    var custom = document.getElementById("link4000")
 
 
-
+if(parseInt(localStorage.getItem("user_stage"))< 13){
+    shop.href="javascript:void(0)"
+    custom.href="javascript:void(0)"
+    shop.style.backgroundColor="gray"
+    custom.style.backgroundColor="gray"
+}
