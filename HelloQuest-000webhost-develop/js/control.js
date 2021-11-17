071e4eb35ff963bd5fac32044f208335ec512e9a
Gelimaster <50572505+Gelimaster@users.noreply.github.com>
2021-11-17 17:09:54 +0900

    [修正]ステージコントロール直した

--- HelloQuest-000webhost-develop/js/control.js
+++ HelloQuest-000webhost-develop/js/control.js
@@ -1,6 +1,6 @@
  var b= document.getElementById("b")
 var stage = document.getElementById("stage")
-const now_stage=localStorage.getItem("user_stage")
+const now_stage=localStorage.getItem("stage")
 var nstage = parseInt(now_stage[1])
 var nstage0 =parseInt(now_stage[0])
 stage.innerHTML=now_stage[0] + "-" + now_stage[1]
@@ -13,18 +13,18 @@ if(nstage == 1 && nstage0==1){
 function back(){
   if(nstage ==1){
  b.style.display=""
-    localStorage.setItem("user_stage",parseInt(now_stage)-10+2)
+    localStorage.setItem("stage",parseInt(now_stage)-10+2)
   }else{
-    localStorage.setItem("user_stage",parseInt(now_stage)-1)
+    localStorage.setItem("stage",parseInt(now_stage)-1)
   }
 location.reload()
 }
 
 function next(){
 if(nstage==3){
-  localStorage.setItem("user_stage",parseInt(now_stage)+10-2)
+  localStorage.setItem("stage",parseInt(now_stage)+10-2)
 }else{
-  localStorage.setItem("user_stage",parseInt(now_stage)+1)
+  localStorage.setItem("stage",parseInt(now_stage)+1)
 }
   location.reload()
 }
